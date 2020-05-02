5191b11c27437c3d5004a18c4fa769eb1b2e0f32
ynynl <yinyin331@gmail.com>
2020-05-02 04:47:32 -0400

    implemented iterator.

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -2,10 +2,10 @@ package synthesizer;
 
 import synthesizer.BoundedQueue;
 
-public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T>, Iterable<T> {
+public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
 //    abstract void moveTo(double deltaX, double deltaY);
     protected int fillCount;
     protected int capacity;
-    public int capacity() {return capacity;}
-    public int fillCount() {return  fillCount;}
+    public int capacity() { return capacity; }
+    public int fillCount() { return  fillCount; }
 }
--- hw1/synthesizer/BoundedQueue.java
+++ hw1/synthesizer/BoundedQueue.java
@@ -1,6 +1,6 @@
 package synthesizer;
 
-public interface BoundedQueue<T> {
+public interface BoundedQueue<T> extends Iterable<T> {
     int capacity();     // return size of the buffer
     int fillCount();    // return number of items currently in the buffer
     void enqueue(T x);  // add item x to the end
@@ -12,5 +12,5 @@ public interface BoundedQueue<T> {
     }
     default boolean isFull()  {   // is the buffer full (fillCount is same as capacity)?
         return (fillCount() == capacity());
-        }
+    }
 }
